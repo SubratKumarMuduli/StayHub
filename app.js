c60e3d08468010e4ae4a952ba17cf5f070dd12a8
Subrat Kumar Muduli <subratmuduli20131219@gmail.com>
2026-08-16 19:59:52 +0530

    Add style using Bootstrap

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const listing = require("./models/listing.js");
 const path = require("path");
+const ejsMate = require("ejs-mate");
 
 app.use(methodOverride("_method"));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
+app.engine('ejs', ejsMate);
+app.use(express.static("public"));
+app.use(express.static(path.join(__dirname,"/public")));
 
 async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/StayHub");
