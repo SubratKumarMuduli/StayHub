a25d2783293b7b69be072001c173055201d27b86
Subrat Kumar Muduli <subratmuduli20131219@gmail.com>
2026-08-22 20:09:09 +0530

    Add Error handling

--- app.js
+++ app.js
@@ -5,14 +5,16 @@ const methodOverride = require("method-override");
 const listing = require("./models/listing.js");
 const path = require("path");
 const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
 
 app.use(methodOverride("_method"));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
-app.engine('ejs', ejsMate);
+app.engine("ejs", ejsMate);
 app.use(express.static("public"));
-app.use(express.static(path.join(__dirname,"/public")));
+app.use(express.static(path.join(__dirname, "/public")));
 
 async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/StayHub");
@@ -30,63 +32,97 @@ app.listen(3000, () => {
   console.log("app is listening at port 3000");
 });
 
+// Home Route
 app.get("/", (req, res) => {
   res.send("Hi, I am root");
 });
 
-app.get("/listings", (req, res) => {
-  listing.find().then((result) => {
-    res.render("listings/index", { result });
-  });
-});
+// Index Route
+app.get(
+  "/listings",
+  wrapAsync(async (req, res) => {
+    await listing.find().then((result) => {
+      res.render("listings/index", { result });
+    });
+  }),
+);
 
 app.get("/listings/new", (req, res) => {
   res.render("listings/new.ejs");
 });
 
-app.get("/listings/:id", async (req, res) => {
-  let { id } = req.params;
-  let list = await listing.findOne({ _id: `${id}` });
-  res.render("listings/view.ejs", { list });
-});
+app.get(
+  "/listings/:id",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    let list = await listing.findOne({ _id: `${id}` });
+    res.render("listings/view.ejs", { list });
+  }),
+);
 
-app.post("/listings/new", async (req, res) => {
-  const { titl, desc, img, pric, loc, contry } = req.body;
-  await listing.insertOne({
-    title: titl,
-    description: desc,
-    image: img,
-    price: pric,
-    location: loc,
-    country: contry,
-  });
-  res.redirect("/listings");
-});
-
-app.get("/listings/:id/edit", async(req, res) => {
-  let { id } = req.params;
-  let list = await listing.findOne({ _id: `${id}` });
-  res.render("listings/edit.ejs", { list,id });
-});
-
-app.put("/listings/:id/edit", async (req, res) => {
-  let { id } = req.params;
-  const { titl, desc, img, pric, loc, contry } = req.body;
-  await listing.findByIdAndUpdate(id, {
-    $set: {
+// Create Route
+app.post(
+  "/listings/new",
+  wrapAsync(async (req, res, next) => {
+    const { titl, desc, img, pric, loc, contry } = req.body;
+    await listing.insertOne({
       title: titl,
       description: desc,
       image: img,
       price: pric,
       location: loc,
       country: contry,
-    },
-  });
-  res.redirect("/listings");
+    });
+    res.redirect("/listings");
+  }),
+);
+
+app.get(
+  "/listings/:id/edit",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    let list = await listing.findOne({ _id: `${id}` });
+    res.render("listings/edit.ejs", { list, id });
+  }),
+);
+
+// Edit Route
+app.put(
+  "/listings/:id/edit",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    const { titl, desc, img, pric, loc, contry } = req.body;
+    await listing.findByIdAndUpdate(id, {
+      $set: {
+        title: titl,
+        description: desc,
+        image: img,
+        price: pric,
+        location: loc,
+        country: contry,
+      },
+    });
+    res.redirect("/listings");
+  }),
+);
+
+// Delete Route
+app.delete(
+  "/listings/:id",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    await listing.findByIdAndDelete(id);
+    res.redirect("/listings");
+  }),
+);
+
+app.use((req, res, next) => {
+  next(new ExpressError(404, "Page not found!"));
 });
 
-app.delete("/listings/:id", async (req, res) => {
-  let {id} = req.params;
-  await listing.findByIdAndDelete(id);
-  res.redirect("/listings");
+// Error Handling Middleware
+app.use((err, req, res, next) => {
+  let { status = 500, message = "Something went wrong" } = err;
+  // res.status(status).send(message);
+  res.status(status).render("error.ejs",{message});
 });
--- models/listing.js
+++ models/listing.js
@@ -3,9 +3,11 @@ const mongoose = require("mongoose");
 const listingSchema = new mongoose.Schema({
   title: {
     type: String,
+    required: true,
   },
   description: {
     type: String,
+    required: true,
   },
   image: {
     type: String,
@@ -18,12 +20,15 @@ const listingSchema = new mongoose.Schema({
   },
   price: {
     type: Number,
+    required: true,
   },
   location: {
     type: String,
+    required: true,
   },
   country: {
     type: String,
+    required: true,
   },
 });
 
